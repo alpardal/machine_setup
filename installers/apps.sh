6ccda4a696176490798f8b4a3ffc6fb428d0fd21
André Luiz Pardal <alpardal@gmail.com>
2017-07-05 09:25:45 -0300

    add Hack font, remove duplicated curl

--- installers/apps.sh
+++ installers/apps.sh
@@ -3,7 +3,7 @@
 sudo add-apt-repository ppa:indicator-multiload/stable-daily -y
 sudo apt-get update
 
-apps="curl tree tmux transmission xtrlock indicator-multiload silversearcher-ag virtualbox okular okular-extra-backends"
+apps="tree tmux transmission xtrlock indicator-multiload silversearcher-ag virtualbox okular okular-extra-backends"
 
 for app in $apps; do
   sudo apt-get install $app -y
--- installers/fonts.sh
+++ installers/fonts.sh
@@ -9,3 +9,8 @@ curl $roboto -L -o ~/.fonts/Roboto\ Mono\ for\ Powerline\ Regular.ttf
 
 ubuntu_mono='https://github.com/powerline/fonts/blob/master/UbuntuMono/Ubuntu%20Mono%20derivative%20Powerline.ttf?raw=true'
 curl $ubuntu_mono -L -o ~/.fonts/Ubuntu\ Mono\ derivative\ Powerline\ Regular.ttf
+
+hack='https://github.com/powerline/fonts/blob/master/Hack/Hack-Regular.ttf?raw=true'
+curl $hack -L -o ~/.fonts/Hack\ derivative\ Powerline\ Regular.ttf
+
+sudo fc-cache -f -v
