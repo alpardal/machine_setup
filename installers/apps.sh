307caa282d92f4fcd7afd9c72e439cc0c4a03592
André Luiz Pardal <alpardal@gmail.com>
2017-07-22 20:45:15 -0300

    add vlc

--- installers/apps.sh
+++ installers/apps.sh
@@ -3,7 +3,7 @@
 sudo add-apt-repository ppa:indicator-multiload/stable-daily -y
 sudo apt-get update
 
-apps="transmission xtrlock indicator-multiload silversearcher-ag virtualbox okular okular-extra-backends arc-theme"
+apps="transmission xtrlock indicator-multiload silversearcher-ag virtualbox okular okular-extra-backends arc-theme vlc"
 
 for app in $apps; do
   sudo apt-get install $app -y
