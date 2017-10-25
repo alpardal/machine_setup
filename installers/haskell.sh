b82c0e79f65537aa7faaba8ecc50439323895cc9
André Luiz Pardal <alpardal@gmail.com>
2017-10-25 20:27:19 -0200

    update haskell setup

--- installers/haskell.sh
+++ installers/haskell.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install haskell-platform -y
-cabal update
+# sudo apt-get install haskell-platform -y
+# cabal update
+curl -sSL https://get.haskellstack.org/ | sh
+stack setup
 stack upgrade
+stack install hlint ghc-mod
