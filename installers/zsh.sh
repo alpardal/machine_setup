0061734028f76a81c903265a479c41d62e2c650b
André Luiz Pardal <alpardal@gmail.com>
2018-01-09 18:00:35 -0200

    update zsh theme

--- installers/zsh.sh
+++ installers/zsh.sh
@@ -9,5 +9,5 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 themes_dir=$HOME/.oh-my-zsh/custom/themes/
 mkdir -p $themes_dir
 
-theme_url='https://raw.githubusercontent.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme'
-curl $theme_url -o $themes_dir/bullet-train.zsh-theme
+theme_url='https://raw.githubusercontent.com/skylerlee/zeta-zsh-theme/master/zeta.zsh-theme'
+curl $theme_url -o $themes_dir/zeta.zsh-theme
