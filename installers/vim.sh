a9c441b166a41b23699a8e8c8e0087ce16df44c8
André Luiz Pardal <alpardal@gmail.com>
2022-03-28 12:48:01 -0300

    vim setup: update deps; install vim-plug

--- installers/vim.sh
+++ installers/vim.sh
@@ -11,6 +11,7 @@ fi
 sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+    make clang libtool-bin libxt-dev libpython3-dev \
     python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev -y
 
 git clone https://github.com/vim/vim.git
@@ -20,3 +21,6 @@ ln -fs $HOME/dotfiles/run_vim_configure_script $HOME/bin/run_vim_configure_scrip
 cd vim
 make
 sudo make install
+
+# install vim-plug:
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \\n    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
